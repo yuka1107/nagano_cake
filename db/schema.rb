f0f5f7b8cfb2e531f1a966904d5b83f6ab226b10
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2021-12-06 08:55:48 +0000

    [Add] Itemモデルとテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_084732) do
+ActiveRecord::Schema.define(version: 2021_12_06_085439) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -51,6 +51,17 @@ ActiveRecord::Schema.define(version: 2021_12_06_084732) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.string "image_id"
+    t.text "introduction"
+    t.integer "price"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

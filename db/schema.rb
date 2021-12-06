508f67c618a255ee9cf0926b0d2cd9785a06e728
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2021-12-06 09:02:39 +0000

    [Add]Cart_itemitemモデルとテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_06_085736) do
+ActiveRecord::Schema.define(version: 2021_12_06_090118) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2021_12_06_085736) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "customer_id"
+    t.integer "mount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
